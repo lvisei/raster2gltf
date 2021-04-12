747cd1075ed9c9c3e4705f3bcd85648809b10a03
liuvigongzuoshi <liuvigongzuoshi@foxmail.com>
2021-04-12 11:34:58 +0800

    tool: test data

---
+++ test/mesh.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+cells = np.array([[0, 1, 2], [0, 1, 3]])
+
+print(cells)
+
+print(cells+1)
+
+
+# print(cells[:, ::-1])
+
+# 定义一个奇异矩阵 A
+# A = np.zeros((4, 4))
+# A[0, -1] = 1
+# A[-1, 0] = -1
+# A = np.matrix(A)
+# print(A)
